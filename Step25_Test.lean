a5c912a73f22cde9efab2220f8048facd0bec902
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-06-06 15:39:45 +0900

    Update Step25_Test.lean

--- Step25_Test.lean
+++ Step25_Test.lean
@@ -349,7 +349,28 @@ lemma fullTerminalRules_type_eq
     H.h [a] =
       (extractedProfile G H
         ({ val := X, property := hsurv } : TrimmedState G H)).yt := by
-  sorry
+  classical
+  unfold fullTerminalRules at hmem
+  cases List.mem_flatMap.mp hmem with
+  | intro rule h1 =>
+    cases h1 with
+    | intro _hrule h2 =>
+      cases rule with
+      | mk A a0 =>
+        cases List.mem_flatMap.mp h2 with
+        | intro m h3 =>
+          cases h3 with
+          | intro _hm h4 =>
+            cases List.mem_map.mp h4 with
+            | intro n h5 =>
+              cases h5 with
+              | intro _hn heq =>
+                simp only [Prod.mk.injEq] at heq
+                cases heq with
+                | intro hX ha =>
+                  cases hX
+                  cases ha
+                  simp [extractedProfile, mkFullTypedState]
 
 lemma fullBinaryRules_yield_eq
     (G : SSBNFGrammar Sigma)
@@ -365,7 +386,41 @@ lemma fullBinaryRules_yield_eq
         ({ val := Y, property := hYsurv } : TrimmedState G H)).yt *
       (extractedProfile G H
         ({ val := Z, property := hZsurv } : TrimmedState G H)).yt := by
-  sorry
+  classical
+  unfold fullBinaryRules at hmem
+  cases List.mem_flatMap.mp hmem with
+  | intro rule h1 =>
+    cases h1 with
+    | intro _hrule h2 =>
+      cases rule with
+      | mk A rest =>
+        cases rest with
+        | mk B C =>
+          cases List.mem_flatMap.mp h2 with
+          | intro m h3 =>
+            cases h3 with
+            | intro _hm h4 =>
+              cases List.mem_flatMap.mp h4 with
+              | intro n h5 =>
+                cases h5 with
+                | intro _hn h6 =>
+                  cases List.mem_flatMap.mp h6 with
+                  | intro q h7 =>
+                    cases h7 with
+                    | intro _hq h8 =>
+                      cases List.mem_map.mp h8 with
+                      | intro r h9 =>
+                        cases h9 with
+                        | intro _hr heq =>
+                          simp only [Prod.mk.injEq] at heq
+                          cases heq with
+                          | intro hX hYZ =>
+                            cases hYZ with
+                            | intro hY hZ =>
+                              cases hX
+                              cases hY
+                              cases hZ
+                              simp [extractedProfile, mkFullTypedState]
 
 lemma fullBinaryRules_left_child_left_eq
     (G : SSBNFGrammar Sigma)
@@ -379,7 +434,41 @@ lemma fullBinaryRules_left_child_left_eq
       ({ val := Y, property := hYsurv } : TrimmedState G H)).lt =
     (extractedProfile G H
       ({ val := X, property := hXsurv } : TrimmedState G H)).lt := by
-  sorry
+  classical
+  unfold fullBinaryRules at hmem
+  cases List.mem_flatMap.mp hmem with
+  | intro rule h1 =>
+    cases h1 with
+    | intro _hrule h2 =>
+      cases rule with
+      | mk A rest =>
+        cases rest with
+        | mk B C =>
+          cases List.mem_flatMap.mp h2 with
+          | intro m h3 =>
+            cases h3 with
+            | intro _hm h4 =>
+              cases List.mem_flatMap.mp h4 with
+              | intro n h5 =>
+                cases h5 with
+                | intro _hn h6 =>
+                  cases List.mem_flatMap.mp h6 with
+                  | intro q h7 =>
+                    cases h7 with
+                    | intro _hq h8 =>
+                      cases List.mem_map.mp h8 with
+                      | intro r h9 =>
+                        cases h9 with
+                        | intro _hr heq =>
+                          simp only [Prod.mk.injEq] at heq
+                          cases heq with
+                          | intro hX hYZ =>
+                            cases hYZ with
+                            | intro hY hZ =>
+                              cases hX
+                              cases hY
+                              cases hZ
+                              simp [extractedProfile, mkFullTypedState]
 
 lemma fullBinaryRules_left_child_right_eq
     (G : SSBNFGrammar Sigma)
@@ -395,7 +484,41 @@ lemma fullBinaryRules_left_child_right_eq
         ({ val := Z, property := hZsurv } : TrimmedState G H)).yt *
       (extractedProfile G H
         ({ val := X, property := hXsurv } : TrimmedState G H)).rt := by
-  sorry
+  classical
+  unfold fullBinaryRules at hmem
+  cases List.mem_flatMap.mp hmem with
+  | intro rule h1 =>
+    cases h1 with
+    | intro _hrule h2 =>
+      cases rule with
+      | mk A rest =>
+        cases rest with
+        | mk B C =>
+          cases List.mem_flatMap.mp h2 with
+          | intro m h3 =>
+            cases h3 with
+            | intro _hm h4 =>
+              cases List.mem_flatMap.mp h4 with
+              | intro n h5 =>
+                cases h5 with
+                | intro _hn h6 =>
+                  cases List.mem_flatMap.mp h6 with
+                  | intro q h7 =>
+                    cases h7 with
+                    | intro _hq h8 =>
+                      cases List.mem_map.mp h8 with
+                      | intro r h9 =>
+                        cases h9 with
+                        | intro _hr heq =>
+                          simp only [Prod.mk.injEq] at heq
+                          cases heq with
+                          | intro hX hYZ =>
+                            cases hYZ with
+                            | intro hY hZ =>
+                              cases hX
+                              cases hY
+                              cases hZ
+                              simp [extractedProfile, mkFullTypedState]
 
 lemma fullBinaryRules_right_child_left_eq
     (G : SSBNFGrammar Sigma)
@@ -411,7 +534,41 @@ lemma fullBinaryRules_right_child_left_eq
         ({ val := X, property := hXsurv } : TrimmedState G H)).lt *
       (extractedProfile G H
         ({ val := Y, property := hYsurv } : TrimmedState G H)).yt := by
-  sorry
+  classical
+  unfold fullBinaryRules at hmem
+  cases List.mem_flatMap.mp hmem with
+  | intro rule h1 =>
+    cases h1 with
+    | intro _hrule h2 =>
+      cases rule with
+      | mk A rest =>
+        cases rest with
+        | mk B C =>
+          cases List.mem_flatMap.mp h2 with
+          | intro m h3 =>
+            cases h3 with
+            | intro _hm h4 =>
+              cases List.mem_flatMap.mp h4 with
+              | intro n h5 =>
+                cases h5 with
+                | intro _hn h6 =>
+                  cases List.mem_flatMap.mp h6 with
+                  | intro q h7 =>
+                    cases h7 with
+                    | intro _hq h8 =>
+                      cases List.mem_map.mp h8 with
+                      | intro r h9 =>
+                        cases h9 with
+                        | intro _hr heq =>
+                          simp only [Prod.mk.injEq] at heq
+                          cases heq with
+                          | intro hX hYZ =>
+                            cases hYZ with
+                            | intro hY hZ =>
+                              cases hX
+                              cases hY
+                              cases hZ
+                              simp [extractedProfile, mkFullTypedState]
 
 lemma fullBinaryRules_right_child_right_eq
     (G : SSBNFGrammar Sigma)
@@ -425,7 +582,41 @@ lemma fullBinaryRules_right_child_right_eq
       ({ val := Z, property := hZsurv } : TrimmedState G H)).rt =
     (extractedProfile G H
       ({ val := X, property := hXsurv } : TrimmedState G H)).rt := by
-  sorry
+  classical
+  unfold fullBinaryRules at hmem
+  cases List.mem_flatMap.mp hmem with
+  | intro rule h1 =>
+    cases h1 with
+    | intro _hrule h2 =>
+      cases rule with
+      | mk A rest =>
+        cases rest with
+        | mk B C =>
+          cases List.mem_flatMap.mp h2 with
+          | intro m h3 =>
+            cases h3 with
+            | intro _hm h4 =>
+              cases List.mem_flatMap.mp h4 with
+              | intro n h5 =>
+                cases h5 with
+                | intro _hn h6 =>
+                  cases List.mem_flatMap.mp h6 with
+                  | intro q h7 =>
+                    cases h7 with
+                    | intro _hq h8 =>
+                      cases List.mem_map.mp h8 with
+                      | intro r h9 =>
+                        cases h9 with
+                        | intro _hr heq =>
+                          simp only [Prod.mk.injEq] at heq
+                          cases heq with
+                          | intro hX hYZ =>
+                            cases hYZ with
+                            | intro hY hZ =>
+                              cases hX
+                              cases hY
+                              cases hZ
+                              simp [extractedProfile, mkFullTypedState]
 
 lemma terminal_mem_extractedR
     (G : SSBNFGrammar Sigma)
@@ -441,7 +632,25 @@ lemma terminal_mem_extractedR
           (And
             (tr.a = a)
             (List.Mem (CarrierTypedRule.terminal tr) (extractedR G H)))) := by
-  sorry
+  classical
+  let Xt : TrimmedState G H := { val := X, property := hsurv }
+  have htype : H.h [a] = (extractedProfile G H Xt).yt :=
+    fullTerminalRules_type_eq G H X a hmem hsurv
+  let tr : CarrierTerminalRule H (extractedProfile G H) :=
+    { X := Xt, a := a, type_eq := htype }
+  apply Exists.intro tr
+  apply And.intro
+  exact rfl
+  apply And.intro
+  exact rfl
+  unfold extractedR
+  apply List.mem_append_left
+  apply List.mem_filterMap.mpr
+  apply Exists.intro (X, a)
+  apply And.intro
+  exact hmem
+  dsimp only
+  rw [dif_pos hsurv, dif_pos htype]
 
 lemma binary_mem_extractedR
     (G : SSBNFGrammar Sigma)
@@ -460,10 +669,60 @@ lemma binary_mem_extractedR
             (And
               (br.Z = ({ val := Z, property := hZsurv } : TrimmedState G H))
               (List.Mem (CarrierTypedRule.binary br) (extractedR G H))))) := by
-  sorry
+  classical
+  let Xt : TrimmedState G H := { val := X, property := hXsurv }
+  let Yt : TrimmedState G H := { val := Y, property := hYsurv }
+  let Zt : TrimmedState G H := { val := Z, property := hZsurv }
 
-end Extraction
+  have hyield :
+      (extractedProfile G H Xt).yt =
+        (extractedProfile G H Yt).yt * (extractedProfile G H Zt).yt :=
+    fullBinaryRules_yield_eq G H X Y Z hmem hXsurv hYsurv hZsurv
 
-end
+  have hleft_left :
+      (extractedProfile G H Yt).lt =
+        (extractedProfile G H Xt).lt :=
+    fullBinaryRules_left_child_left_eq G H X Y Z hmem hXsurv hYsurv hZsurv
 
-end TwoSidedTypedCFG
+  have hleft_right :
+      (extractedProfile G H Yt).rt =
+        (extractedProfile G H Zt).yt * (extractedProfile G H Xt).rt :=
+    fullBinaryRules_left_child_right_eq G H X Y Z hmem hXsurv hYsurv hZsurv
+
+  have hright_left :
+      (extractedProfile G H Zt).lt =
+        (extractedProfile G H Xt).lt * (extractedProfile G H Yt).yt :=
+    fullBinaryRules_right_child_left_eq G H X Y Z hmem hXsurv hYsurv hZsurv
+
+  have hright_right :
+      (extractedProfile G H Zt).rt =
+        (extractedProfile G H Xt).rt :=
+    fullBinaryRules_right_child_right_eq G H X Y Z hmem hXsurv hYsurv hZsurv
+
+  let br : CarrierBinaryRule (extractedProfile G H) :=
+    { X := Xt
+      Y := Yt
+      Z := Zt
+      yield_eq := hyield
+      left_child_left_eq := hleft_left
+      left_child_right_eq := hleft_right
+      right_child_left_eq := hright_left
+      right_child_right_eq := hright_right }
+
+  apply Exists.intro br
+  apply And.intro
+  exact rfl
+  apply And.intro
+  exact rfl
+  apply And.intro
+  exact rfl
+  unfold extractedR
+  apply List.mem_append_right
+  apply List.mem_filterMap.mpr
+  apply Exists.intro (X, Y, Z)
+  apply And.intro
+  exact hmem
+  dsimp only
+  rw [dif_pos hXsurv, dif_pos hYsurv, dif_pos hZsurv,
+      dif_pos hyield, dif_pos hleft_left, dif_pos hleft_right,
+      dif_pos hright_left, dif_pos hright_right]
