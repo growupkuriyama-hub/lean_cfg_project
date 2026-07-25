f338ddc69342943e9ce739a7df6c129bef5aa563
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-07-25 15:16:25 +0900

    7

--- LeanCfgProject/MCFG2/CharacteristicSampleExactOnceSuccessfulRuleRealizationRoute.lean
+++ LeanCfgProject/MCFG2/CharacteristicSampleExactOnceSuccessfulRuleRealizationRoute.lean
@@ -1,3 +1,4 @@
+-- CI rerun marker: no semantic change; preserves the latest repairs.
 /-
 Copyright (c) 2026 Takayuki Kuriyama. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
