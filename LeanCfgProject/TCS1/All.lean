ab2d101dc5445995db0cddd657573de67dd80db6
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 08:28:50 +0900

    TCS1 v68: integrate marked-boundary kernel

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -14,6 +14,7 @@ import LeanCfgProject.TCS1.FixedWindowCharacteristicDataBounds
 import LeanCfgProject.TCS1.FixedWindowTreeCombinatorics
 import LeanCfgProject.TCS1.FixedWindowSummarySemantic
 import LeanCfgProject.TCS1.FixedWindowBoundarySemantic
+import LeanCfgProject.TCS1.MarkedBoundaryKernel
 import LeanCfgProject.TCS1.ShortestNonemptyPathBound
 import LeanCfgProject.TCS1.ShortestNonemptySpineSemantic
 import LeanCfgProject.TCS1.FiniteSupportSpineBound
