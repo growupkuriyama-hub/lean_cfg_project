3b5c0fcd546aaa2f99f8cd6e540ddc35ed18dfc6
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 06:18:45 +0900

    TCS1 v66: derive shortest-yield bounds from finite grammar support

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -14,6 +14,7 @@ import LeanCfgProject.TCS1.FixedWindowCharacteristicDataBounds
 import LeanCfgProject.TCS1.FixedWindowTreeCombinatorics
 import LeanCfgProject.TCS1.ShortestNonemptyPathBound
 import LeanCfgProject.TCS1.ShortestNonemptySpineSemantic
+import LeanCfgProject.TCS1.FiniteSupportSpineBound
 import LeanCfgProject.TCS1.FixedWindowContextPathBound
 
 import LeanCfgProject.TCS1.TerminalIsolationKernel
