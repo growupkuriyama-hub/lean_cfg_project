731a07c790d9aa3cc36f7094b4dd9856619ba238
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 08:33:41 +0900

    TCS1 v68: integrate marked-boundary normalization

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -15,6 +15,7 @@ import LeanCfgProject.TCS1.FixedWindowTreeCombinatorics
 import LeanCfgProject.TCS1.FixedWindowSummarySemantic
 import LeanCfgProject.TCS1.FixedWindowBoundarySemantic
 import LeanCfgProject.TCS1.MarkedBoundaryKernel
+import LeanCfgProject.TCS1.MarkedBoundaryNormalization
 import LeanCfgProject.TCS1.ShortestNonemptyPathBound
 import LeanCfgProject.TCS1.ShortestNonemptySpineSemantic
 import LeanCfgProject.TCS1.FiniteSupportSpineBound
