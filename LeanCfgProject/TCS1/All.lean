f936a1e88e12fb02a382d0ebf8997da95774b489
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 06:52:06 +0900

    TCS1 v66: add linear support closed under suffix tails

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -29,6 +29,7 @@ import LeanCfgProject.TCS1.FrontEndThicknessKernel
 import LeanCfgProject.TCS1.FrontEndBinaryThickness
 import LeanCfgProject.TCS1.FrontEndFiniteSupportFacade
 import LeanCfgProject.TCS1.FiniteCFGEncoding
+import LeanCfgProject.TCS1.IndexedClosedFrontSupport
 import LeanCfgProject.TCS1.NormalizationLanguageBounds
 import LeanCfgProject.TCS1.BinaryEpsilonElimination
 import LeanCfgProject.TCS1.BinaryUnitElimination
