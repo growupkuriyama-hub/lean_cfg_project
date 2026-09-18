e3c5397f5a23fbd8569b01bc52423a4779020d2a
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-18 20:47:12 +0900

    TCS1 v65: add binarization kernel to facade

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -15,6 +15,7 @@ import LeanCfgProject.TCS1.ShortestNonemptyPathBound
 import LeanCfgProject.TCS1.FixedWindowContextPathBound
 
 import LeanCfgProject.TCS1.TerminalIsolationKernel
+import LeanCfgProject.TCS1.BinarizationKernel
 import LeanCfgProject.TCS1.NormalizationLanguageBounds
 import LeanCfgProject.TCS1.BinaryEpsilonElimination
 import LeanCfgProject.TCS1.BinaryUnitElimination
