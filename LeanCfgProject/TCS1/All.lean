5f1cff364cf11afdf40d34100948efff4030e84d
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 13:19:23 +0900

    TCS1 v68: integrate gap-aware head attachment

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -17,6 +17,7 @@ import LeanCfgProject.TCS1.FixedWindowBoundarySemantic
 import LeanCfgProject.TCS1.MarkedBoundaryKernel
 import LeanCfgProject.TCS1.MarkedBoundaryNormalization
 import LeanCfgProject.TCS1.MarkedBoundaryGapSemantic
+import LeanCfgProject.TCS1.MarkedBoundaryGapNormalization
 import LeanCfgProject.TCS1.MarkedBoundarySelection
 import LeanCfgProject.TCS1.FixedWindowBoundaryMarking
 import LeanCfgProject.TCS1.FixedWindowTreeSurgeryFacade
