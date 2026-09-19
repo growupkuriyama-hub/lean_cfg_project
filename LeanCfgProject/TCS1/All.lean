af49df143d90ff91f8f095613aa7bae74d17a721
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 01:41:28 +0900

    Integrate linear normalization soundness

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -57,6 +57,7 @@ import LeanCfgProject.TCS1.LinearNormalizationSize
 import LeanCfgProject.TCS1.LinearNormalizationPlan
 import LeanCfgProject.TCS1.PreparedLinearGrammarSemantics
 import LeanCfgProject.TCS1.LinearNormalizationRules
+import LeanCfgProject.TCS1.LinearNormalizationSoundness
 import LeanCfgProject.TCS1.LinearReducedWitnessBridge
 import LeanCfgProject.TCS1.LinearCharacteristicDataFacade
 import LeanCfgProject.TCS1.ConcreteLinearCharacteristicData
