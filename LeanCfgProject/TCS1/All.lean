4479275d6804e8ce8f88b27df2ec8e657ab31a5f
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 01:30:45 +0900

    Integrate concrete linear normalization rules

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -55,6 +55,7 @@ import LeanCfgProject.TCS1.LinearSpineSemantic
 import LeanCfgProject.TCS1.LinearTypedShapeBridge
 import LeanCfgProject.TCS1.LinearNormalizationSize
 import LeanCfgProject.TCS1.LinearNormalizationPlan
+import LeanCfgProject.TCS1.LinearNormalizationRules
 import LeanCfgProject.TCS1.LinearReducedWitnessBridge
 import LeanCfgProject.TCS1.LinearCharacteristicDataFacade
 import LeanCfgProject.TCS1.ConcreteLinearCharacteristicData
