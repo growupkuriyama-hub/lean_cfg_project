84e369b56eba7a8f641f3779f857513e37d93c7e
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 14:53:02 +0900

    TCS1 v68: integrate Lemma 7.2 witness bound facade

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -23,6 +23,7 @@ import LeanCfgProject.TCS1.MarkedBoundarySelection
 import LeanCfgProject.TCS1.FixedWindowBoundaryMarking
 import LeanCfgProject.TCS1.FixedWindowTreeSurgeryFacade
 import LeanCfgProject.TCS1.FixedWindowLemma71ReducedFacade
+import LeanCfgProject.TCS1.FixedWindowLemma72Facade
 import LeanCfgProject.TCS1.ShortestNonemptyPathBound
 import LeanCfgProject.TCS1.ShortestNonemptySpineSemantic
 import LeanCfgProject.TCS1.FiniteSupportSpineBound
