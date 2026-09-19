812f55db4c9e586b7b342184ae7a2409c1460994
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 16:16:47 +0900

    TCS1 v68: integrate canonical witness family counting

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -4,6 +4,7 @@ import LeanCfgProject.TCS1.ReconstructionSoundness
 import LeanCfgProject.TCS1.CanonicalWitnessCompleteness
 import LeanCfgProject.TCS1.ConservativeGoldKernel
 import LeanCfgProject.TCS1.WitnessSetConstruction
+import LeanCfgProject.TCS1.CanonicalWitnessCounting
 import LeanCfgProject.TCS1.ReducednessWitnessChoices
 import LeanCfgProject.TCS1.MinimalReducedWitnessChoices
 import LeanCfgProject.TCS1.GoldConvergenceClosure
