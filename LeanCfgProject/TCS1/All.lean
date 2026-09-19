4f45d4334e67650a8479e72ef0598f3ffd75f033
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 00:59:13 +0900

    Integrate typed linear-spine shape bridge

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -52,6 +52,7 @@ import LeanCfgProject.TCS1.ReducedTypedRuleBridge
 import LeanCfgProject.TCS1.ShortestNonemptyPathBound
 import LeanCfgProject.TCS1.LinearSpineBounds
 import LeanCfgProject.TCS1.LinearSpineSemantic
+import LeanCfgProject.TCS1.LinearTypedShapeBridge
 import LeanCfgProject.TCS1.LinearReducedWitnessBridge
 import LeanCfgProject.TCS1.LinearCharacteristicDataFacade
 import LeanCfgProject.TCS1.ConcreteLinearCharacteristicData
