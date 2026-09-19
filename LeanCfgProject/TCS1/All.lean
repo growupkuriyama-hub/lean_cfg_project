fa3f7b0d84da1d74bd3e3c4bc3cdc8350fa4d21d
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 22:04:56 +0900

    Integrate concrete conservative learner

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -10,6 +10,7 @@ import LeanCfgProject.TCS1.ReducednessWitnessChoices
 import LeanCfgProject.TCS1.MinimalReducedWitnessChoices
 import LeanCfgProject.TCS1.GoldConvergenceClosure
 import LeanCfgProject.TCS1.GoldCharacteristicBridge
+import LeanCfgProject.TCS1.ConcreteConservativeLearner
 import LeanCfgProject.TCS1.IndexedGoldBridge
 
 import LeanCfgProject.TCS1.SSBNFThicknessBounds
