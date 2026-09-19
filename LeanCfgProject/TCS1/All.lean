31e9b686481cbcd7f4ebf0c14817bf66d49e509c
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 22:49:19 +0900

    Integrate concrete learner complexity bookkeeping

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -12,6 +12,7 @@ import LeanCfgProject.TCS1.MinimalReducedWitnessChoices
 import LeanCfgProject.TCS1.GoldConvergenceClosure
 import LeanCfgProject.TCS1.GoldCharacteristicBridge
 import LeanCfgProject.TCS1.ConcreteConservativeLearner
+import LeanCfgProject.TCS1.ConcreteLearnerComplexity
 import LeanCfgProject.TCS1.TrivialTargetEndpoints
 import LeanCfgProject.TCS1.IndexedGoldBridge
 import LeanCfgProject.TCS1.IndexedConcreteGoldLearner
