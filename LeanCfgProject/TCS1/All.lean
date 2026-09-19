d9bcb985ffd06d9ff3b953bbea2511d80df3a634
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 16:54:44 +0900

    TCS1 v68: integrate the concrete fixed-window monoid

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -17,6 +17,7 @@ import LeanCfgProject.TCS1.FixedWindowCharacteristicDataFacade
 import LeanCfgProject.TCS1.FixedWindowTreeCombinatorics
 import LeanCfgProject.TCS1.FixedWindowSummarySemantic
 import LeanCfgProject.TCS1.FixedWindowConcreteSummary
+import LeanCfgProject.TCS1.FixedWindowConcreteMonoid
 import LeanCfgProject.TCS1.FixedWindowBoundarySemantic
 import LeanCfgProject.TCS1.MarkedBoundaryKernel
 import LeanCfgProject.TCS1.MarkedBoundaryNormalization
