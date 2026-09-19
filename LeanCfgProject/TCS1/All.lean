889cc709ed2d71c0b8097b616f9507420767a298
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-19 16:47:59 +0900

    TCS1 v68: integrate concrete fixed-window summary states

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -16,6 +16,7 @@ import LeanCfgProject.TCS1.FixedWindowCharacteristicDataBounds
 import LeanCfgProject.TCS1.FixedWindowCharacteristicDataFacade
 import LeanCfgProject.TCS1.FixedWindowTreeCombinatorics
 import LeanCfgProject.TCS1.FixedWindowSummarySemantic
+import LeanCfgProject.TCS1.FixedWindowConcreteSummary
 import LeanCfgProject.TCS1.FixedWindowBoundarySemantic
 import LeanCfgProject.TCS1.MarkedBoundaryKernel
 import LeanCfgProject.TCS1.MarkedBoundaryNormalization
