94a9c0b38c70db1af577650f2049d6a0b5b7449d
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 00:40:02 +0900

    Integrate linear-spine semantic bridge

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -51,6 +51,7 @@ import LeanCfgProject.TCS1.ConcreteTypedTrimLanguage
 import LeanCfgProject.TCS1.ReducedTypedRuleBridge
 import LeanCfgProject.TCS1.ShortestNonemptyPathBound
 import LeanCfgProject.TCS1.LinearSpineBounds
+import LeanCfgProject.TCS1.LinearSpineSemantic
 import LeanCfgProject.TCS1.LinearCanonicalWitnessBounds
 import LeanCfgProject.TCS1.ShortestNonemptySpineSemantic
 import LeanCfgProject.TCS1.FiniteSupportSpineBound
