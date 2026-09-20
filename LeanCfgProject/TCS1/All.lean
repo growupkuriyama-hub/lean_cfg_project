a554fc9334667ffd3d13081a5e1ea9b1c1062043
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-21 03:29:12 +0900

    TCS1 v78: add boundary-factor kernel to full facade

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -7,6 +7,7 @@ import LeanCfgProject.TCS1.LinearSeparatorDistribution
 import LeanCfgProject.TCS1.LinearSeparatorPowerContexts
 import LeanCfgProject.TCS1.LinearSeparatorFactorSlices
 import LeanCfgProject.TCS1.LinearSeparatorContextShape
+import LeanCfgProject.TCS1.LinearSeparatorBoundaryFactors
 import LeanCfgProject.TCS1.ClarkCongruentialKernel
 import LeanCfgProject.TCS1.YieldTypedRefinementCore
 import LeanCfgProject.TCS1.ReconstructionSoundness
