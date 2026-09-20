9330fff02e9fbce8ccc59f263992515fcf9878e1
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-21 02:32:20 +0900

    Integrate final untyped useful trim

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -53,6 +53,7 @@ import LeanCfgProject.TCS1.ShortestNonemptyPathBound
 import LeanCfgProject.TCS1.LinearSpineBounds
 import LeanCfgProject.TCS1.LinearSpineSemantic
 import LeanCfgProject.TCS1.LinearTypedShapeBridge
+import LeanCfgProject.TCS1.UntypedUsefulTrim
 import LeanCfgProject.TCS1.LinearNormalizationSize
 import LeanCfgProject.TCS1.LinearNormalizationPlan
 import LeanCfgProject.TCS1.PreparedLinearGrammarSemantics
