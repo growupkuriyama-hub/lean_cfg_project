6fd7265c0f7d3b417c0645c9b2e448d650a28df1
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-21 03:37:14 +0900

    TCS1 v78: integrate Proposition 8.6 core facade

--- LeanCfgProject/TCS1/V77Delta.lean
+++ LeanCfgProject/TCS1/V77Delta.lean
@@ -10,6 +10,7 @@ import LeanCfgProject.TCS1.LinearSeparatorContextShape
 import LeanCfgProject.TCS1.LinearSeparatorBoundaryFactors
 import LeanCfgProject.TCS1.LinearSeparatorBalance
 import LeanCfgProject.TCS1.LinearSeparatorFixedH
+import LeanCfgProject.TCS1.LinearSeparatorProposition86
 import LeanCfgProject.TCS1.ClarkCongruentialKernel
 
 /-!
