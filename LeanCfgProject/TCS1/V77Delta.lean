388397abccd138d19645ffa5d367588d2f89bc5e
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 20:40:02 +0900

    TCS1 v77: add Clark kernel to delta facade

--- LeanCfgProject/TCS1/V77Delta.lean
+++ LeanCfgProject/TCS1/V77Delta.lean
@@ -2,6 +2,7 @@ import LeanCfgProject.TCS1.FixedWindowExactEquivalence
 import LeanCfgProject.TCS1.FixedHFiniteObstructions
 import LeanCfgProject.TCS1.LinearSeparatorExample
 import LeanCfgProject.TCS1.LinearSeparatorTyping
+import LeanCfgProject.TCS1.ClarkCongruentialKernel
 
 /-!
 # TCS #1 v77 delta verification facade
