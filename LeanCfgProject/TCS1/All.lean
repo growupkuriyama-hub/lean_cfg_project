42317d16a39ed352b7bfda82610b45f5e7f2b2fd
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 22:48:53 +0900

    Integrate arbitrary linear characteristic-data theorem

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -72,6 +72,7 @@ import LeanCfgProject.TCS1.LinearRawUnitFreePreparedBridge
 import LeanCfgProject.TCS1.IndexedLinearNormalizationFacade
 import LeanCfgProject.TCS1.IndexedLinearNormalizationSize
 import LeanCfgProject.TCS1.IndexedLinearNormalizationTheorem
+import LeanCfgProject.TCS1.IndexedLinearCharacteristicData
 import LeanCfgProject.TCS1.LinearRawFinitePreprocessing
 import LeanCfgProject.TCS1.LinearReducedWitnessBridge
 import LeanCfgProject.TCS1.LinearCharacteristicDataFacade
