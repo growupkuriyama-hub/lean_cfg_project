0b93a0816032239126cae80bd1b09d77b32ae3a1
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 21:05:19 +0900

    TCS1 v77: integrate separator power-context kernel

--- LeanCfgProject/TCS1/V77Delta.lean
+++ LeanCfgProject/TCS1/V77Delta.lean
@@ -3,6 +3,7 @@ import LeanCfgProject.TCS1.FixedHFiniteObstructions
 import LeanCfgProject.TCS1.LinearSeparatorExample
 import LeanCfgProject.TCS1.LinearSeparatorTyping
 import LeanCfgProject.TCS1.LinearSeparatorDistribution
+import LeanCfgProject.TCS1.LinearSeparatorPowerContexts
 import LeanCfgProject.TCS1.ClarkCongruentialKernel
 
 /-!
