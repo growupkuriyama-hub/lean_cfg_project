fdeee47b313337a09903c6d3cbe6a3c37df28cfd
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-21 05:02:10 +0900

    TCS1 v78: integrate finite-monoid obstruction kernel

--- LeanCfgProject/TCS1/V77Delta.lean
+++ LeanCfgProject/TCS1/V77Delta.lean
@@ -1,5 +1,6 @@
 import LeanCfgProject.TCS1.FixedWindowExactEquivalence
 import LeanCfgProject.TCS1.FixedHFiniteObstructions
+import LeanCfgProject.TCS1.FiniteMonoidObstructionKernel
 import LeanCfgProject.TCS1.FixedHRightQuotient
 import LeanCfgProject.TCS1.LinearSeparatorExample
 import LeanCfgProject.TCS1.LinearSeparatorTyping
