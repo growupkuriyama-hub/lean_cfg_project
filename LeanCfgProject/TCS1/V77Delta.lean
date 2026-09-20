df0a9c6cc9018c917b2c00509154af33eff9f049
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-21 03:31:41 +0900

    TCS1 v78: add separator fixed-h theorem to delta facade

--- LeanCfgProject/TCS1/V77Delta.lean
+++ LeanCfgProject/TCS1/V77Delta.lean
@@ -9,6 +9,7 @@ import LeanCfgProject.TCS1.LinearSeparatorFactorSlices
 import LeanCfgProject.TCS1.LinearSeparatorContextShape
 import LeanCfgProject.TCS1.LinearSeparatorBoundaryFactors
 import LeanCfgProject.TCS1.LinearSeparatorBalance
+import LeanCfgProject.TCS1.LinearSeparatorFixedH
 import LeanCfgProject.TCS1.ClarkCongruentialKernel
 
 /-!
