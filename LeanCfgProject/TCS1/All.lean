fd6498bc51035ef617441006b846f6529c8076a9
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 17:15:22 +0900

    Integrate constructed linear start-language bridge

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -62,6 +62,7 @@ import LeanCfgProject.TCS1.LinearNormalizationCompleteness
 import LeanCfgProject.TCS1.PreparedLinearNormalizationFacade
 import LeanCfgProject.TCS1.LinearMixedRhsDecomposition
 import LeanCfgProject.TCS1.LinearIndexedPreprocessedBridge
+import LeanCfgProject.TCS1.LinearConstructedStartLanguage
 import LeanCfgProject.TCS1.LinearReducedWitnessBridge
 import LeanCfgProject.TCS1.LinearCharacteristicDataFacade
 import LeanCfgProject.TCS1.ConcreteLinearCharacteristicData
