cc8424e765109eda8549c343e0895ab83e53c7a2
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 19:09:29 +0900

    Integrate paper-facing arbitrary linear normalization theorem

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -71,6 +71,7 @@ import LeanCfgProject.TCS1.LinearRawPreprocessingFacade
 import LeanCfgProject.TCS1.LinearRawUnitFreePreparedBridge
 import LeanCfgProject.TCS1.IndexedLinearNormalizationFacade
 import LeanCfgProject.TCS1.IndexedLinearNormalizationSize
+import LeanCfgProject.TCS1.IndexedLinearNormalizationTheorem
 import LeanCfgProject.TCS1.LinearRawFinitePreprocessing
 import LeanCfgProject.TCS1.LinearReducedWitnessBridge
 import LeanCfgProject.TCS1.LinearCharacteristicDataFacade
