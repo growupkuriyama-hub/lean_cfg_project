251f59f4daaee9d0c1840041a6e42fecea03fa34
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-20 16:09:25 +0900

    Integrate prepared linear normalization facade

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -59,6 +59,7 @@ import LeanCfgProject.TCS1.PreparedLinearGrammarSemantics
 import LeanCfgProject.TCS1.LinearNormalizationRules
 import LeanCfgProject.TCS1.LinearNormalizationSoundness
 import LeanCfgProject.TCS1.LinearNormalizationCompleteness
+import LeanCfgProject.TCS1.PreparedLinearNormalizationFacade
 import LeanCfgProject.TCS1.LinearReducedWitnessBridge
 import LeanCfgProject.TCS1.LinearCharacteristicDataFacade
 import LeanCfgProject.TCS1.ConcreteLinearCharacteristicData
