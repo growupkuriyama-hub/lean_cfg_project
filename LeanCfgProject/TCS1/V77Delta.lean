2e28d8ebd94951d560bd2fab3a5c93aa30a5d578
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-21 14:52:25 +0900

    TCS1 v78: integrate one-bracket Dyck kernel in delta build

--- LeanCfgProject/TCS1/V77Delta.lean
+++ LeanCfgProject/TCS1/V77Delta.lean
@@ -1,6 +1,7 @@
 import LeanCfgProject.TCS1.FixedWindowExactEquivalence
 import LeanCfgProject.TCS1.FixedHFiniteObstructions
 import LeanCfgProject.TCS1.FiniteMonoidObstructionKernel
+import LeanCfgProject.TCS1.DyckOneBracketKernel
 import LeanCfgProject.TCS1.FixedHRightQuotient
 import LeanCfgProject.TCS1.LinearSeparatorExample
 import LeanCfgProject.TCS1.LinearSeparatorTyping
