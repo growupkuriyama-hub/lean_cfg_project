64f25b4519317df3240afe272dca49e26a6da4d4
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-21 15:13:02 +0900

    TCS1 v78: integrate Dyck congruential grammar in full build

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -2,6 +2,7 @@ import LeanCfgProject.TCS1.FixedHSubstitutability
 import LeanCfgProject.TCS1.FixedHFiniteObstructions
 import LeanCfgProject.TCS1.FiniteMonoidObstructionKernel
 import LeanCfgProject.TCS1.DyckOneBracketKernel
+import LeanCfgProject.TCS1.DyckOneBracketGrammar
 import LeanCfgProject.TCS1.FixedHRightQuotient
 import LeanCfgProject.TCS1.LinearSeparatorExample
 import LeanCfgProject.TCS1.LinearSeparatorTyping
