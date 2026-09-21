fd7eeb36447a0ebf6ce2c5e9532283e0293f361d
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-21 21:28:12 +0900

    TCS1 v78: integrate Delta-star finite CFG witness

--- LeanCfgProject/TCS1/V77Delta.lean
+++ LeanCfgProject/TCS1/V77Delta.lean
@@ -13,6 +13,7 @@ import LeanCfgProject.TCS1.DeltaStarFixedH
 import LeanCfgProject.TCS1.DeltaStarFixedHSubstitutability
 import LeanCfgProject.TCS1.DeltaStarNonregular
 import LeanCfgProject.TCS1.DeltaStarDisplayedGrammar
+import LeanCfgProject.TCS1.DeltaStarBinaryGrammar
 import LeanCfgProject.TCS1.DyckOneBracketKernel
 import LeanCfgProject.TCS1.DyckOneBracketGrammar
 import LeanCfgProject.TCS1.LukasiewiczBoundary
