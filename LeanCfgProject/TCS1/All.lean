140097934228d973a78cd3fb2b12ec8f0a71c47e
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-22 06:48:48 +0900

    TCS1 v79: integrate linear pumping kernel

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -95,6 +95,7 @@ import LeanCfgProject.TCS1.ReducedTypedRuleBridge
 import LeanCfgProject.TCS1.ShortestNonemptyPathBound
 import LeanCfgProject.TCS1.LinearSpineBounds
 import LeanCfgProject.TCS1.LinearSpineSemantic
+import LeanCfgProject.TCS1.LinearPumpingKernel
 import LeanCfgProject.TCS1.LinearTypedShapeBridge
 import LeanCfgProject.TCS1.LinearNormalizationSize
 import LeanCfgProject.TCS1.LinearNormalizationPlan
