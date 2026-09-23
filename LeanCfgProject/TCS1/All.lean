4c5b95934db15e6c77437ff77998899d3a49417b
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-23 19:09:23 +0900

    TCS1 v79: integrate reconstruction CFG presentation

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -42,6 +42,7 @@ import LeanCfgProject.TCS1.LinearSeparatorPreparedGrammar
 import LeanCfgProject.TCS1.ClarkCongruentialKernel
 import LeanCfgProject.TCS1.YieldTypedRefinementCore
 import LeanCfgProject.TCS1.ReconstructionSoundness
+import LeanCfgProject.TCS1.ReconstructionCFGPresentation
 import LeanCfgProject.TCS1.ReconstructionComplexityCounts
 import LeanCfgProject.TCS1.ReconstructionFiniteCandidateSpaces
 import LeanCfgProject.TCS1.BatchLanguageMonotonicity
