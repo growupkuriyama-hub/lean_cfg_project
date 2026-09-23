83d0f8d936b38cab2f4cb23162ec1688d809fa79
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-23 16:39:31 +0900

    TCS1 v79: integrate bounded CYK round semantics

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -56,6 +56,7 @@ import LeanCfgProject.TCS1.GoldCharacteristicBridge
 import LeanCfgProject.TCS1.ConcreteConservativeLearner
 import LeanCfgProject.TCS1.ConcreteLearnerComplexity
 import LeanCfgProject.TCS1.BinaryMembershipKernel
+import LeanCfgProject.TCS1.BinaryMembershipRounds
 import LeanCfgProject.TCS1.TrivialTargetEndpoints
 import LeanCfgProject.TCS1.IndexedGoldBridge
 import LeanCfgProject.TCS1.IndexedConcreteGoldLearner
