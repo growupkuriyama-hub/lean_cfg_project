ecd23bdc3977fc25c2fd870591fece480d9028fe
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-23 17:44:22 +0900

    TCS1 v79: integrate CYK completeness helpers

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -60,6 +60,7 @@ import LeanCfgProject.TCS1.BinaryMembershipRounds
 import LeanCfgProject.TCS1.BinaryMembershipChart
 import LeanCfgProject.TCS1.BinaryMembershipSeed
 import LeanCfgProject.TCS1.BinaryMembershipChartSound
+import LeanCfgProject.TCS1.BinaryMembershipChartComplete
 import LeanCfgProject.TCS1.TrivialTargetEndpoints
 import LeanCfgProject.TCS1.IndexedGoldBridge
 import LeanCfgProject.TCS1.IndexedConcreteGoldLearner
