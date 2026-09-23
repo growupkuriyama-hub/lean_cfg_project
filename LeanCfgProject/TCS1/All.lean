91e5a6f83c59be4f431c36139efcf41ec5cb1133
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-23 20:39:16 +0900

    TCS1 v79: integrate executable factor-slot CYK learner

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -50,6 +50,7 @@ import LeanCfgProject.TCS1.ReconstructionCYKBridge
 import LeanCfgProject.TCS1.FiniteUnitReachability
 import LeanCfgProject.TCS1.ReconstructionFactorSlotState
 import LeanCfgProject.TCS1.ReconstructionFactorSlotGrammar
+import LeanCfgProject.TCS1.ReconstructionFactorSlotCYK
 import LeanCfgProject.TCS1.ReconstructionComplexityCounts
 import LeanCfgProject.TCS1.ReconstructionFiniteCandidateSpaces
 import LeanCfgProject.TCS1.BatchLanguageMonotonicity
