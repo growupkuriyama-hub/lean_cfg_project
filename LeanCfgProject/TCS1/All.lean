bb14f01e6bacae4d287c891060e0c2d2cc945975
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-23 16:56:42 +0900

    TCS1 v79: integrate CYK terminal seed

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -58,6 +58,7 @@ import LeanCfgProject.TCS1.ConcreteLearnerComplexity
 import LeanCfgProject.TCS1.BinaryMembershipKernel
 import LeanCfgProject.TCS1.BinaryMembershipRounds
 import LeanCfgProject.TCS1.BinaryMembershipChart
+import LeanCfgProject.TCS1.BinaryMembershipSeed
 import LeanCfgProject.TCS1.TrivialTargetEndpoints
 import LeanCfgProject.TCS1.IndexedGoldBridge
 import LeanCfgProject.TCS1.IndexedConcreteGoldLearner
