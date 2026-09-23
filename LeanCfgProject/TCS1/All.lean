1d1aecea980e9baa38b24e2c89297f063cf8d000
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-23 18:33:37 +0900

    TCS1 v79: integrate conservative CYK cost bridge

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -63,6 +63,7 @@ import LeanCfgProject.TCS1.BinaryMembershipChartSound
 import LeanCfgProject.TCS1.BinaryMembershipChartComplete
 import LeanCfgProject.TCS1.BinaryMembershipChartExact
 import LeanCfgProject.TCS1.BinaryMembershipDecision
+import LeanCfgProject.TCS1.ConservativeMembershipCost
 import LeanCfgProject.TCS1.TrivialTargetEndpoints
 import LeanCfgProject.TCS1.IndexedGoldBridge
 import LeanCfgProject.TCS1.IndexedConcreteGoldLearner
