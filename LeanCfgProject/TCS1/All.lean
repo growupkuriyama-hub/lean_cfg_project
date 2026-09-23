08e1b51c4d26fc7e473d60c8947dacb20de021cb
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-23 21:04:40 +0900

    TCS1 v79: integrate executable main theorem package

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -79,6 +79,7 @@ import LeanCfgProject.TCS1.IndexedGoldBridge
 import LeanCfgProject.TCS1.IndexedConcreteGoldLearner
 import LeanCfgProject.TCS1.IndexedFixedHBridge
 import LeanCfgProject.TCS1.MainTheoremSemanticPackage
+import LeanCfgProject.TCS1.MainTheoremExecutablePackage
 
 import LeanCfgProject.TCS1.SSBNFThicknessBounds
 import LeanCfgProject.TCS1.SSBNFNormalizationCombinatorics
