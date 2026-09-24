d88d57289506980e385a8cd89ae037b2aab91408
growupkuriyama-hub <growup.kuriyama@gmail.com>
2026-09-24 13:35:35 +0900

    TCS1 v79: integrate materialized production cost

--- LeanCfgProject/TCS1/All.lean
+++ LeanCfgProject/TCS1/All.lean
@@ -54,6 +54,7 @@ import LeanCfgProject.TCS1.ReconstructionFactorSlotCYK
 import LeanCfgProject.TCS1.ReconstructionProductionTables
 import LeanCfgProject.TCS1.ExecutableConservativeLearner
 import LeanCfgProject.TCS1.MaterializedConservativeLearner
+import LeanCfgProject.TCS1.MaterializedProductionCost
 import LeanCfgProject.TCS1.ExecutableSmokeTest
 import LeanCfgProject.TCS1.ReconstructionComplexityCounts
 import LeanCfgProject.TCS1.ReconstructionFiniteCandidateSpaces
